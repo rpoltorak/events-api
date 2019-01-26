44eff9aa6993a150867ad16c0b1a54e91560d0d6
Remek Poltorak <rpoltorak@entitive.com>
2019-01-26 16:06:59 +0100

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Application } from 'express';
+import express, { Application } from "express";
 
 const app: Application = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
