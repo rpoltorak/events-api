0093e5074f5cccec8be11d159ac8c957031de132
Remek Poltorak <rpoltorak@entitive.com>
2019-01-26 19:28:44 +0100

    PostgreSQL database initialization

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express, { Application } from "express";
+import { createConnection } from "typeorm";
+import "reflect-metadata";
 
-const app: Application = express();
+createConnection().then(async connection => {
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  const app: Application = express();
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Example app listening on port 3000!");
-});
+  app.get("/", async (req, res) => {
+    res.send("Hello world");
+  });
+
+  app.listen(process.env.PORT || 3000, async () => {
+    console.log("Example app listening on port 3000!");
+  });
+
+}).catch(error => console.log("Database connection error", error));;
